69ec39d77b98540a42060c6533648ba0826ad78d
ksalcce <62475617+ksalcce@users.noreply.github.com>
2020-10-21 16:57:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
+// Store object creates a object location in which we can pull the location property in correlation from the github api location property to correlate with the openweather location property
+const store ={
+  location:null
+};
+
+// when submitting a name of a company in the repo search app it will submit the query and fetch both API's in correlation to the search value 
 $("#orgSearchForm").submit(function(e){
-    e.preventDefault();
-    const query = $('#q').val();
-    getDataFromApi(`https://api.github.com/orgs/${query}`);
-    debugger;
-  })
-  
-  
-  function getDataFromApi(url){
-    fetch(url)
-      .then((response) => response.json())
-      .then(data => displayResults(data, '#orgData'))
-  
-    $.get( url, function( data ) {
-      
-      displayOrgResults(data, '#orgData');
-      $( '#orgLogo' ).attr({src: data.avatar_url});
-  
-    });
+  e.preventDefault();
+  const query = $('#q').val();
+  // get data from API using FETCH
+  getDataFromApi(`https://api.github.com/orgs/${query}`, (data) => {           
+    store.location = data.location;
+
+    displayOrgResults(data, '#orgData');
+    getDataFromApi(`https://api.openweathermap.org/data/2.5/weather?q=${data.location}&appid=8a9c9363c957db6b62f36d894c48f97d`,(data) => displayWeather(data) );
+
+  });
+  debugger;
+})
+
+// function definition that grabs the data from the OpenWeather API and displays the outcome dependent on the search value's location from the Github api. It will then check if status code and weather can be found. If not it display message below
+function displayWeather(data){
+  debugger; 
+  // if/else statement check if city not found.
+  // if city not found display sorry cant find weather for store.location
+
+  if(data.cod !== 200){
+    $('#weather').html(`Sorry Cant find weather in ${store.location} !!`);
+  }else{
+  $('#weather').html(data.weather[0].description);
   }
   
-  
-  function displayOrgResults(data, targetQuery){
-    const target = $(targetQuery);
-    target.html(OrgDataComponent(data));
-  
-  }
-  
-  function OrgDataComponent(data) {
-    return `<h4><strong>Company: </strong> ${data.name}</h4>
-            <p><strong>Location: </strong> ${data.location}</p>
-            <p><strong>Total Repos: </strong> ${data.public_repos}</p>`;
-  }
+}
+
+// function definition that fetches and calls the API to get data from either API and distuinguishes a callback function in response when the API is found and loaded successfully
+function getDataFromApi(url,callback){
+  fetch(url)
+    .then((response) => response.json())
+    .then(callback)
+    
+}
+ 
+//  function call takes the data and targetQuery of Github API  to display the weather on the app 
+function displayOrgResults(data, targetQuery){
+  const target = $(targetQuery);
+ $( '#orgLogo' ).attr({src: data.avatar_url});
+  target.html(OrgDataComponent(data));
+
+
+}
+
+// function call takes the appropriate data to fill in the right fields of the app from Github API
+function OrgDataComponent(data) {
+  return `<h4><strong>Company: </strong> ${data.name}</h4>
+          <p><strong>Location: </strong> ${data.location}</p>
+           <p><strong>Weather: <span id="weather"></span></strong>
+          <p><strong>Total Repos: </strong> ${data.public_repos}</p>`;
+}
